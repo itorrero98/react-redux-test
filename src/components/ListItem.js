2ae348c3b3c439170330ead5d4df46d82b2fe822
itorrero98 <issiactorrero@u.boisestate.edu>
2019-07-16 16:40:38 -0600

    Added an action to store which library the user tapped on

---
+++ src/components/ListItem.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
+class ListItem extends Component {
+    render() {
+        const { id, title } = this.props.library.item;
+        const { titleStyle } = styles;
+        return (
+            <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+
+                </View>
+            </TouchableWithoutFeedback>
+        )
+    }
+}
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
+};
+
+export default connect(null, actions)(ListItem);
